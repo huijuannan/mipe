c62da1c70d84e8cef98e81483778c750016328bb
Alan Lai <techwplai@gmail.com>
2015-12-16 21:09:55 +0800

    add get_links()

--- GitbookMonitor/GitbookMonitor.py
+++ GitbookMonitor/GitbookMonitor.py
@@ -1,4 +1,7 @@
 import MySQLdb
+import urllib2
+from urlparse import urljoin
+from bs4 import BeautifulSoup
 
 def init_database():
     conn = MySQLdb.connect(host='127.0.0.1',unix_socket='/tmp/mysql.sock',
@@ -45,5 +48,27 @@ def init_database():
     cur.close()
     conn.close()
 
+def get_links(url):
+    """get all chapter links from a gitbook url
+    
+    Arg:
+        url :
+
+    Return:
+
+    """
+    req = urllib2.Request(url)
+    handler = urllib2.urlopen(req)
+    html = handler.read()
+
+    soup = BeautifulSoup(html)
+    tags = soup.find_all('li',class_='chapter')
+    links = [urljoin(url, tag.a.attrs['href']) 
+             for tag in tags]
+    
+    return links 
+
 if __name__ == '__main__':
-    init_database()
+    url = "http://localhost:4000/"
+    print get_links(url)
+   
